8f548175a656fb189b765bca7354052131b6eac3
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-16 23:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function trailingZeros(n) {
-    let count = 0;
+    n = Number(n);   // 🔥 important fix
 
+    let count = 0;
     for (let i = 5; i <= n; i *= 5) {
         count += Math.floor(n / i);
     }
-
     return count;
 }
 
-let num = Number(prompt());
+let num = prompt();
 alert(trailingZeros(num));
-
-
-const input = prompt("Enter a number");
-alert(trailingZeros(input));
